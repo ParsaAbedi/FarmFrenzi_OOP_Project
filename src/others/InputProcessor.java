48b202ade2e0e563200d4eeb6de34bb3799b8458
Parsa <mo.parsaabedi@gmail.com>
2021-05-28 08:35:09 +0430

    false commit

--- src/others/InputProcessor.java
+++ src/others/InputProcessor.java
@@ -1,5 +1,18 @@
 package others;
-
+import java.util.regex.Pattern;
+import java.util.Scanner;
 public class InputProcessor {
-    private int num = 0;
+    private Scanner sc = new Scanner(System.in);
+    private Manager manager = new Manager();
+    String command;
+    boolean quit = false;
+    void run()
+    {
+        while(!quit)
+        {
+            command = sc.nextLine();
+            quit = true;
+        }
+    }
+
 }
