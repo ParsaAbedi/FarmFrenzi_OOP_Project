35df37bb0c3935de9eccd16ffb29cf373a48dfd6
Parsa <mo.parsaabedi@gmail.com>
2021-05-29 15:03:46 +0430

    Inputprocessor is now almost completed (when you complete a task some non related message shows up) I have also created empty command methods in manager and add Setting menu too but it is still empty

--- src/others/Manager.java
+++ src/others/Manager.java
@@ -15,4 +15,54 @@ public class Manager {
     public boolean checkPassword(String username , String command) {
         return true;
     }
+
+    public boolean truckGo() {
+        //TODO
+        return true;
+    }
+
+    public boolean unloadTruck(String s) {
+        //TODO
+        return true;
+    }
+
+    public boolean loadTruck(String s) {
+        //TODO
+        return true;
+    }
+
+    public boolean turn(String s) {
+        //TODO
+        return true;
+    }
+
+    public boolean cage(String s, String s1) {
+        //TODO
+        return true;
+    }
+
+    public boolean work(String s) {
+        //TODO
+        return true;
+    }
+
+    public boolean plant(String s, String s1) {
+        //TODO
+        return true;
+    }
+
+    public boolean well() {
+        //TODO
+        return true;
+    }
+
+    public boolean pickup(String s, String s1) {
+        //TODO
+        return true;
+    }
+
+    public boolean buy(String s) {
+        //TODO
+        return true;
+    }
 }
