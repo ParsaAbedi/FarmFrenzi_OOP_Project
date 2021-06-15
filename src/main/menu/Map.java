8dfb5463f1b43d0cdac45ba42fcc64319bae4cd3
Parsa <mo.parsaabedi@gmail.com>
2021-06-15 16:45:53 +0430

    "Mission menu added! Mission class and task class added! now program read mission from file and some related methods were added in manager"

---
+++ src/main/menu/Map.java
@@ -0,0 +1,82 @@
+package main.menu;
+
+import others.Manager;
+import others.Mission;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class Map extends Menu{
+    private Manager manager = new Manager();
+    ArrayList<Mission> missions = new ArrayList<>(manager.loadMissions());
+    public Map(Menu parent) {
+        super("MAIN MENU");
+        logger.start();
+        HashMap<Integer, Menu> submenus = new HashMap<>();
+        for (int i=0 ; i< manager.missionsNum ; i++)
+        {
+            submenus.put(i+1,new InputProcessor(this));
+        }
+        this.setSubmenus(submenus);
+    }
+    public void show() {
+        System.out.println(super.getName() + ": ");
+        for(int i=0 ; i< manager.missionsNum ; i++)
+        {
+            submenus.get(i+1).setName("MISSION "+(i+1));
+        }
+        for (Integer integer : submenus.keySet()) {
+            System.out.println(integer + ". " + submenus.get(integer).getName());
+        }
+        if (this.parentMenu == null) {
+            System.out.println((submenus.size() + 1) + ". exit");
+        } else {
+            System.out.println((submenus.size() + 1) + ". back");
+        }
+    }
+    public void execute (){
+        Menu nextMenu = null;
+        String command;
+        boolean quit = false;
+        int nextMenuNum ;
+        while (!quit)
+        {
+            command = scanner.nextLine();
+            command = command.trim();
+            if(Pattern.matches("[1-9]" , command))
+            {
+                nextMenuNum = Integer.parseInt(command);
+                if (nextMenuNum == submenus.size() + 1) {
+                    if (this.parentMenu == null) {
+                        logger.writeInfo("System exit!");
+                        System.exit(1);
+                    }
+                    else {
+                        nextMenu = parentMenu;
+                        quit = true;
+                    }
+                }
+                else if (nextMenuNum < submenus.size() + 1 && nextMenuNum > 0) {
+                    logger.writeInfo("Submenu number "+nextMenuNum+" selected!");
+                    mission = missions.get(nextMenuNum-1);
+                    System.out.println(mission.toString());
+                    nextMenu = submenus.get(nextMenuNum);
+                    quit = true;
+                }
+                else
+                {
+                    logger.writeError("Invalid input in "+this.name);
+                    System.err.println("Invalid input!");
+                }
+            }
+            else {
+                logger.writeError("Invalid input in "+this.name);
+                System.err.println("Invalid input!");
+            }
+
+        }
+        nextMenu.show();
+        nextMenu.execute();
+    }
+}
