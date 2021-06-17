3b0d12257ab388eb1a2811b6588c1e60bc3bbc88
Parsa <mo.parsaabedi@gmail.com>
2021-06-17 11:53:02 +0430

    finally merged!

--- src/others/Task.java
+++ src/others/Task.java
@@ -3,8 +3,30 @@ package others;
 public class Task {
     String definition ;
     boolean isDone = false;
-
-    public Task(String definition) {
+    int value ;
+    String type;
+    public Task(String definition , int value) {
         this.definition = definition;
+        this.value = value;
+    }
+    public boolean isCompleted()
+    {
+        return true;
+    }
+    public String showTask()
+    {
+        return definition;
+    }
+
+    @Override
+    public String toString() {
+        return "Task definitio = " +
+                definition +
+                "\nTask isDone = " +
+                isDone +
+                "\nTask value = " +
+                value +
+                "\nTask type = " +
+                type;
     }
 }
